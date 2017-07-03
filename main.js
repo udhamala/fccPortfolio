6f463e0e8e044302a5a9006ebe186d9ef1e9877b
Uttam Dhamala <USA@Uttams-MBP.attlocal.net>
2017-07-02 22:33:03 -0700

    Portfolio for freecodecamp project

---
+++ main.js
@@ -0,0 +1,26 @@
+// function initMap() {
+//   var uluru = {lat: -25.363, lng: 131.044};
+//   var map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: uluru
+//   });
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map
+//   });
+// }
+// function initialize() {
+//   var map = new google.maps.Map(
+//     document.getElementById("map"), {
+//       center: new google.maps.LatLng(37.4419, -122.1419),
+//       zoom: 13,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//   });
+
+//   var marker = new google.maps.Marker({
+//         position: new google.maps.LatLng(37.4419, -122.1419),
+//         map: map
+//   });
+
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
